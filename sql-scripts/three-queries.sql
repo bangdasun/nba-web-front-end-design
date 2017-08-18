4b21d3b228b3e909df1a02a8a2d88b61d1344763
Bangda Sun <bangdasun94@gmail.com>
2017-08-18 19:34:27 -0400

    Update and rename three queries.sql to sql-scripts/three-queries.sql

--- sql-scripts/three-queries.sql
+++ sql-scripts/three-queries.sql
@@ -1,11 +1,14 @@
+-- 1. 
 SELECT pname
 FROM player
 WHERE (drafts).pick = 1;
 
+-- 2.
 SELECT array_length(honors, 1) 
 FROM player
 WHERE pname = 'LeBron James';
 
+-- 3.
 SELECT to_tsvector(transac_history) @@ 'heat'
 FROM player
 WHERE pname = 'LeBron James';
